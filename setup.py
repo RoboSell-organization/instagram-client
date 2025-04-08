926cffe92457f1c35386c2135df2aee37c362189
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2025-04-08 15:18:57 +0500

    fix: fixed python versioning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="instagram-client",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     author="Abdulvoris",
     author_email="erkinovabdulvoris101@gmail.com",
@@ -21,5 +21,5 @@ setup(
         "pydantic>=1.9.0",
         "typing_extensions>=4.0.0"
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.11",
 )
